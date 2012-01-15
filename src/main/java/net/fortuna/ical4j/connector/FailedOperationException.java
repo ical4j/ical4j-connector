8d09d720b5db9b6b644e084a46446099aca34ed8
fortuna <fortuna@users.sourceforge.net>
2012-01-15 12:50:30 +1100

    Added license plugin

--- src/main/java/net/fortuna/ical4j/connector/FailedOperationException.java
+++ src/main/java/net/fortuna/ical4j/connector/FailedOperationException.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2011, Ben Fortuna
  * All rights reserved.
  *
@@ -29,7 +29,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package net.fortuna.ical4j.connector;
 
 /**
