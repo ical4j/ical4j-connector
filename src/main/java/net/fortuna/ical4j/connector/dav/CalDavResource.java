d1183add889aaa1210a3a8f520f60a898d85f353
Ben Fortuna <bfortuna@odecee.com.au>
2022-03-22 12:31:58 +1100

    Implement dav resources

---
+++ src/main/java/net/fortuna/ical4j/connector/dav/CalDavResource.java
@@ -0,0 +1,35 @@
+package net.fortuna.ical4j.connector.dav;
+
+import org.apache.jackrabbit.webdav.DavException;
+import org.apache.jackrabbit.webdav.DavResource;
+import org.apache.jackrabbit.webdav.DavResourceFactory;
+import org.apache.jackrabbit.webdav.DavResourceLocator;
+import org.apache.jackrabbit.webdav.io.InputContext;
+import org.apache.jackrabbit.webdav.io.OutputContext;
+import org.apache.jackrabbit.webdav.property.DavPropertySet;
+
+import java.io.IOException;
+
+public class CalDavResource extends AbstractDavResource<CalDavSupport> {
+
+    public CalDavResource(DavResourceFactory factory, DavResourceLocator locator, DavPropertySet properties,
+                          CalDavSupport client, CalDavResource parent) {
+
+        super(factory, locator, properties, client, parent);
+    }
+
+    @Override
+    public void spool(OutputContext outputContext) throws IOException {
+
+    }
+
+    @Override
+    public void addMember(DavResource resource, InputContext inputContext) throws DavException {
+
+    }
+
+    @Override
+    public void removeMember(DavResource member) throws DavException {
+
+    }
+}
