dc85409833673f91ce6dc80af259e3a3b0d84b7f
Ben Fortuna <bfortuna@odecee.com.au>
2022-06-28 09:41:49 +1000

    Support initialisation of client with optional auth credentials

--- ical4j-connector-dav/src/main/java/org/ical4j/connector/dav/DavClientFactory.java
+++ ical4j-connector-dav/src/main/java/org/ical4j/connector/dav/DavClientFactory.java
@@ -1,11 +1,15 @@
 package org.ical4j.connector.dav;
 
+import org.apache.http.client.CredentialsProvider;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DavClientFactory {
 
     private DavClientConfiguration clientConfiguration;
+
+    private CredentialsProvider credentialsProvider;
 
     public DavClientFactory() {
         clientConfiguration = new DavClientConfiguration();
@@ -21,6 +25,11 @@ public class DavClientFactory {
         return this;
     }
 
+    public DavClientFactory withCredentialsProvider(CredentialsProvider credentialsProvider) {
+        this.credentialsProvider = credentialsProvider;
+        return this;
+    }
+
     /**
      * Create a new client instance. Note that if a path is specified it should include a trailing
      * forward slash.
@@ -28,7 +37,13 @@ public class DavClientFactory {
      * @return a new client instance
      */
     public DefaultDavClient newInstance(URL url) {
-        return new DefaultDavClient(url, clientConfiguration);
+        DefaultDavClient client = new DefaultDavClient(url, clientConfiguration);
+        if (credentialsProvider != null) {
+            client.begin(credentialsProvider);
+        } else {
+            client.begin();
+        }
+        return client;
     }
 
     /**
@@ -38,6 +53,12 @@ public class DavClientFactory {
      * @return a new client instance
      */
     public DefaultDavClient newInstance(String url) throws MalformedURLException {
-        return new DefaultDavClient(url, clientConfiguration);
+        DefaultDavClient client = new DefaultDavClient(url, clientConfiguration);
+        if (credentialsProvider != null) {
+            client.begin(credentialsProvider);
+        } else {
+            client.begin();
+        }
+        return client;
     }
 }
